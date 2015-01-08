1631da45c915dad43013e794ba5efce3d79cb971
Brandon Ivey <teenageriot@gmail.com>
2015-01-08 11:15:25 -0500

    heroku stuffin

--- run.py
+++ run.py
@@ -14,8 +14,15 @@ import os
 from eve import Eve
 
 if __name__ == '__main__':
-    port = 5000
-    host = '0.0.0.0'
+    # Heroku support: bind to PORT if defined, otherwise default to 5000.
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT'))
+        # use '0.0.0.0' to ensure your REST API is reachable from all your
+        # network (and not only your computer).
+        host = '0.0.0.0'
+    else:
+        port = 5000
+        host = '0.0.0.0'
 
     app = Eve()
     app.run(host=host, port=port)
